65261b7ed96854c1906570b513c039d334570c9e
Aminat.A <aminatadesola@gmail.com>
2020-07-01 16:15:23 -0500

    added nav component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Navbar/style.css"
+
+function Navbar() {
+
+    return(
+
+        
+        
+<nav class="nav">
+    <a class="navbar-brand" href="#" >
+        <span>Aminat Adesola | Portfolio</span>
+    </a>
+  <a class="nav-link" href="#">Home</a>
+  <a class="nav-link" href="#">Portfolio</a>
+  <a class="nav-link" href="#">Case Studies</a>
+  <a class="nav-link " href="#">Contact</a>
+</nav>
+    )
+}
+
+export default Navbar;
