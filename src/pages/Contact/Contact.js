07ec51807c3f93d0868b11b99266a0f2dd5cd940
Aminat.A <aminatadesola@gmail.com>
2020-07-22 16:53:01 -0500

    working contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,45 +1,72 @@
 import React from "react";
 
+  const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
 
-function Contact (){
+  class Contact extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { name: "", email: "", message: "" };
+    }
 
-  return(
-  <form>
+    /* Here’s the juicy bit for posting the form submission */
 
+    handleSubmit = e => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+        .then(() => alert("Success!"))
+        .catch(error => alert(error));
 
-    <div className="row">
+      e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+      const { name, email, message } = this.state;
+      return (
+        <div className="row">
       <div className="col-6">
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <form onSubmit={this.handleSubmit}>
       <div className=" form-group">
-      <label for="exampleInputEmail1">Email Address</label>
-      <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-      <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div className=" form-group">
-      <label for="exampleInputPassword1">Name</label>
-      <input type="text" name="name" className="form-control" id="exampleInputPassword1"/>
-    </div>
-    <div className="form-group">
-    <label for="exampleFormControlTextarea1">Message</label>
-    <textarea className="form-control" textarea name="message" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+            <label>Your Name:</label>
+            <input className="form-control" type="text" name="name" value={name} onChange={this.handleChange} />
       </div>
-      <div className="col-6 border-0 card" style={{backgroundColor:"#e8f0ff"}}>
-        <div class="card-body">
-          <h2 class="card-title text-center" style={{color:"#9747ff"}}>Get In Touch</h2>
-          <p class="card-text">
-            Feel free to leave a message, even just to say hi. Or look for me on my 
-            other social media platforms.
-          </p>
+        <div className=" form-group">
+            <label> Your Email:</label>
+            <input  className="form-control" type="email" name="email" value={email} onChange={this.handleChange} />
         </div>
+        <div className=" form-group">
+            <label> Message:</label>
+            <textarea  className="form-control" name="message" value={message} onChange={this.handleChange} />
+        </div>
+            <button className="btn btn-primary" type="submit">Send</button>
+        </form>
       </div>
-    
-    </div>
 
-</form>
-  )
-}
+      
+<div className="col-6 border-0 card" style={{backgroundColor:"#e8f0ff"}}>
+  <div class="card-body">
+    <h2 class="card-title text-center" style={{color:"#9747ff"}}>Get In Touch</h2>
+    <p class="card-text">
+      Feel free to leave a message, even just to say hi. Or look for me on my 
+      other social media platforms.
+    </p>
+  </div>
+</div>
+      </div>
+     
+     
+      );
+    }
+  }
+  
+
 
 export default Contact;
