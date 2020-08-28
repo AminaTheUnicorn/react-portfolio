cb83e433e1c62096abfe44fe33c12195722713b2
Aminat.A <aminatadesola@gmail.com>
2020-08-27 21:20:08 -0500

    skillset progressbar changed to icons

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,47 +1,56 @@
 import React from "react";
+import "./styles.css";
 
 function SmCard() {
 
     return(
 <>
 
-<div class="row row-cols-2">
+
+<div class="row row-cols-3">
 
 <div class="col">
         <p>UX Design</p>
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" style={{width: "95%"}} aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">95%</div>
+        <div class="ux">
+            <i class="fas fa-edit"></i>
         </div>
     </div>
 
     <div class="col">
         <p>HTML/CSS</p>
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" style={{width: "90%"}} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
+        <div class="html">
+            <i class="fas fa-file-code"></i>
         </div>
     </div>
 
-    <div class="col mt-4">
+    <div class="col">
         <p>MySQL</p>
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" style={{width: "90%"}} aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
+        <div class="mysql">
+            <i class="fas fa-database"></i> 
         </div>
     </div>
     
-    <div class="col mt-4">
+    <div class="col">
         <p>JavaScript</p>
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" style={{width: "85%"}} aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%</div>
+        <div class="javaS">
+            <i class="fab fa-js-square"></i>
         </div>
     </div>
 
-    <div class="col mt-4">
-        <p>React JS</p>
-        <div class="progress">
-            <div class="progress-bar" role="progressbar" style={{width: "85%"}} aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%</div>
+    <div class="col">
+        <p>React</p>
+        <div class="react">
+            <i class="fab fa-react"></i>
         </div>
     </div>
 
+
+    <div class="col">
+        <p>Node.js</p>
+        <div class="react">
+            <i class="fab fa-node-js"></i>
+        </div>
+    </div>
   
 
 
